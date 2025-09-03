bee49f41af2c7c595aa274f2f01bbbd8e0d2a28b
shinji <shinji@i-elements.net>
2025-09-03 06:06:52 +0000

    ThemeColorTester 見た目調整

--- src/MUITheme/ThemeColorTester.jsx
+++ src/MUITheme/ThemeColorTester.jsx
@@ -3,15 +3,16 @@ import { WAKU } from '../MUITheme/MUITheme'
 import { Box } from '@mui/material'
 
 const ThemeColorTester = () => {
+  const _sx = { m:"3px" , p:"3px 8px",borderRadius:"5px", flex: 1, fontSize: "small",  };
   return (
     <>
     <Box sx={(theme) => ( {...WAKU(theme),display:"flex",flexDirection:"column"})}>
-    <Box sx={{ flex: 1, fontSize: "small", bgcolor: 'primary.main', color: 'primary.contrastText' }}>ThemeColorTester</Box>
-      <Box sx={{ flex: 1, fontSize: "small", bgcolor: 'primary.light', color: 'primary.contrastText' }}>ThemeColorTester</Box>
-      <Box sx={{ flex: 1, fontSize: "small", bgcolor: 'primary.dark', color: 'primary.contrastText' }}>ThemeColorTester</Box>
-      <Box sx={{ flex: 1, fontSize: "small", bgcolor: 'secondary.main', color: 'secondary.contrastText' }}>ThemeColorTester</Box>
-      <Box sx={{ flex: 1, fontSize: "small", bgcolor: 'secondary.light', color: 'secondary.contrastText' }}>ThemeColorTester</Box>
-      <Box sx={{ flex: 1, fontSize: "small", bgcolor: 'secondary.dark', color: 'secondary.contrastText' }}>ThemeColorTester</Box>
+      <Box sx={{..._sx , bgcolor: 'primary.main', color: 'primary.contrastText'}}>ThemeColorTester</Box>
+      <Box sx={{..._sx , bgcolor: 'primary.light', color: 'primary.contrastText' }}>ThemeColorTester</Box>
+      <Box sx={{ ..._sx , bgcolor: 'primary.dark', color: 'primary.contrastText' }}>ThemeColorTester</Box>
+      <Box sx={{ ..._sx , bgcolor: 'secondary.main', color: 'secondary.contrastText' }}>ThemeColorTester</Box>
+      <Box sx={{ ..._sx , bgcolor: 'secondary.light', color: 'secondary.contrastText' }}>ThemeColorTester</Box>
+      <Box sx={{ ..._sx , bgcolor: 'secondary.dark', color: 'secondary.contrastText' }}>ThemeColorTester</Box>
     </Box>
     </>
   )
