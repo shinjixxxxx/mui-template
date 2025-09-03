0f3f659ea4cddfecdaf0d63598627ded524f7414
shinji <shinji@i-elements.net>
2025-09-03 04:24:28 +0000

    コメント調整

--- src/main.jsx
+++ src/main.jsx
@@ -12,9 +12,10 @@ import { NoSsr } from '@mui/material';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ThemeProvider
     theme={theme}
-    defaultMode="dark"      // ← 起動時は OS の設定を見に行く
-    // modeStorageKey={null}         // ← localStorage に保存しない　うまくいかない？
-    storageManager={null}         // ← localStorage に保存しない　同期しない
+    defaultMode="system"          // ← 起動時は OS の設定を見に行く
+    // modeStorageKey={null}    // ← localStorage に保存しない　うまくいかない？
+    storageManager={null}       // ← localStorage に保存しない　同期しない ○
+    // storageManager={null} 「保存しない・同期もしない」 タブごとに完全に独立して扱える
     // disableTransitionOnChange
     noSsr
   >
